39c23f7949ff8f1b35aa60fcce622302b8b2a20b
EA Harper <EAH@EAHarperConsulting.com>
2016-10-25 01:07:55 -0400

    VDOT's Cap & Spd
    Field added to highway network for VDOT to over-ride Free Flow  Speed if desired.

--- CUBE/FreeFlowSpeeds.s
+++ CUBE/FreeFlowSpeeds.s
@@ -1,9 +1,8 @@
 ;;<<Default Template>><<NETWORK>><<Default>>;;
 ; Do not change filenames or add or remove FILEI/FILEO statements using an editor. Use Cube/Application Manager.
 RUN PGM=NETWORK PRNFILE="{SCENARIO_DIR}\OUTPUT\LOGS\FreeflowSpeeds.prn" MSG='Attach Capacity and Free Flow Speed'
-FILEI LOOKUPI[2] = "{CATALOG_DIR}\Params\Spd_Lookup.dbf"
 FILEI LINKI[1] = "{SCENARIO_DIR}\Output\ATYPE NETWORK{Year}{Alternative}.NET"
-FILEI LOOKUPI[1] = "{CATALOG_DIR}\Params\SpdCap_Lookup.dbf"
+FILEI LOOKUPI[1] = "{CATALOG_DIR}\Params\Cap_Lookup.dbf"
 
 FILEO NETO = "{SCENARIO_DIR}\Output\Interim{Year}{Alternative}.NET"
 
@@ -13,17 +12,8 @@ LOOKUP LOOKUPI=1,
        NAME=CAP_LU,
          LOOKUP[1]=LINKID, RESULT=CAP,
          FAIL[1]=0,FAIL[2]=0,FAIL[3]=0,
-         LIST=Y
+         LIST=Y       
 
-; Define LOOKUP FUNCTION
-; Freeflow speed adjustment (applied to Posted Speed) for links based on Facility Type and Land Use Density(1-3)
-LOOKUP LOOKUPI=1,
-       NAME=SPD_LU,
-         LOOKUP[1]=LINKID, RESULT=SPDADJ,
-         FAIL[1]=0,FAIL[2]=0,FAIL[3]=0,
-         LIST=Y
-         
-         
 REPORT DEADLINKS=T, DUPLICATES=T, UNCONNECTED=T MERGE=T
 
 PROCESS  PHASE=LINKMERGE
@@ -37,17 +27,23 @@ IF (LI.1.ATYPE <=2)
     LUD=3
 ENDIF
 
-; Lookup Capacity and Speed
+; Lookup Capacity
 CAPE = (CAP_LU(1,((LI.1.FACTYPE*10) + LUD)))* LANES
-FFSPEED = LI.1.POST_SPD + SPD_LU(1,((LI.1.FACTYPE*10) + LUD))
+FFSPEED = LI.1.POST_SPD
 
 /*
 The lookup table capacities can be over-ridden for a specific link by coding a non-zero 
-  capacity in the VDOT_CAP field on the input network.  This over-ride should be used 
-  cautiously, and the justification should documented by the user. 
+  capacity in the VDOT_CAP field on the input network.  Similarly, the free flow speed can 
+  be over-ridden with a non-zero value in the VDOT_FFSPD field. 
+  
+  This over-ride should be used cautiously, and the justification should documented by the user. 
 */
 IF (LI.1.VDOT_CAP > 0)
     CAPE = LI.1.VDOT_CAP
+ENDIF
+    
+IF (LI.1.VDOT_FFSPD > 0)
+    FFSPEED = LI.1.VDOT_FFSPD    
 ENDIF
 
 ; Capacities by time of day are derived from CAPE with adjustment factors.
